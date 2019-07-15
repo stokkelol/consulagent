85ec79dddef5c995e88989b55c0a39ac7663fe3b
Stokkelol <redwhitepwnz@gmail.com>
2019-07-15 11:14:36 +0300

    Add services struct to agent (#7)

--- agent.go
+++ agent.go
@@ -34,11 +34,11 @@ type Config struct {
 }
 
 type Agent struct {
-	Config  *Config
-	Agent   *consul.Agent
-	Catalog *consul.Catalog
-	KV      *consul.KV
-	Client  *consul.Client
+	config  *Config
+	agent   *consul.Agent
+	catalog *consul.Catalog
+	kv      *consul.KV
+	client  *consul.Client
 }
 
 type CheckFunc func() bool
@@ -88,41 +88,57 @@ func NewAgent(config *Config) (*Agent, error) {
 		return nil, errConfigNotValidated
 	}
 
-	s := &Agent{Config: config}
+	s := &Agent{config: config}
 	err := s.newClient()
 	if err != nil {
 		return nil, err
 	}
 
 	serviceDef := &consul.AgentServiceRegistration{
-		Name: s.Config.ServiceName,
+		Name: s.config.ServiceName,
 		Check: &consul.AgentServiceCheck{
-			TTL: s.Config.TTL.String(),
+			TTL: s.config.TTL.String(),
 		},
-		Port:    s.Config.ContainerPort,
-		Address: s.Config.Address,
-		Tags:    []string{s.Config.Env},
-		ID:      s.Config.ServiceName,
+		Port:    s.config.ContainerPort,
+		Address: s.config.Address,
+		Tags:    []string{s.config.Env},
+		ID:      s.config.ServiceName,
 	}
 
-	if err := s.Agent.ServiceRegister(serviceDef); err != nil {
+	if err := s.agent.ServiceRegister(serviceDef); err != nil {
 		return nil, err
 	}
 
 	return s, nil
 }
 
-func (s *Agent) UpdateTTL(check CheckFunc) {
-	ticker := time.NewTicker(s.Config.TTL / 2)
+func (a *Agent) KV() *consul.KV {
+	return a.kv
+}
+
+func (a *Agent) Client() *consul.Client {
+	return a.client
+}
+
+func (a *Agent) Agent() *consul.Agent {
+	return a.agent
+}
+
+func (a *Agent) Catalog() *consul.Catalog {
+	return a.catalog
+}
+
+func (a *Agent) UpdateTTL(check CheckFunc) {
+	ticker := time.NewTicker(a.config.TTL / 2)
 	for range ticker.C {
-		if err := s.update(check); err != nil {
+		if err := a.update(check); err != nil {
 			return
 		}
 	}
 }
 
-func (s *Agent) LoadKV() (uint64, consul.KVPairs, error) {
-	kvPairs, meta, err := s.KV.List(s.formatPrefix(), nil)
+func (a *Agent) LoadKV() (uint64, consul.KVPairs, error) {
+	kvPairs, meta, err := a.kv.List(a.formatPrefix(), nil)
 	if err != nil {
 		return 0, kvPairs, err
 	}
@@ -130,52 +146,52 @@ func (s *Agent) LoadKV() (uint64, consul.KVPairs, error) {
 	return meta.LastIndex, kvPairs, nil
 }
 
-func (s *Agent) IterateKV(kvPairs consul.KVPairs, creds map[string]string) {
+func (a *Agent) IterateKV(kvPairs consul.KVPairs, creds map[string]string) {
 	for _, kv := range kvPairs {
-		k := s.replaceKey(kv.Key)
+		k := a.replaceKey(kv.Key)
 		if _, ok := creds[k]; ok {
 			creds[k] = string(kv.Value)
 		}
 	}
 }
 
-func (s *Agent) update(check CheckFunc) error {
+func (a *Agent) update(check CheckFunc) error {
 	if !check() {
-		if err := s.Agent.UpdateTTL(s.formatCheckID(), s.Config.FailPhrase, "fail"); err != nil {
+		if err := a.agent.UpdateTTL(a.formatCheckID(), a.config.FailPhrase, "fail"); err != nil {
 			return err
 		}
 	}
 
-	return s.Agent.UpdateTTL(s.formatCheckID(), s.Config.PassPhrase, "pass")
+	return a.agent.UpdateTTL(a.formatCheckID(), a.config.PassPhrase, "pass")
 }
 
-func (s *Agent) newClient() error {
+func (a *Agent) newClient() error {
 	client, err := consul.NewClient(&consul.Config{
-		Address: fmt.Sprintf("%s:%d", s.Config.ConsulAddress, s.Config.AgentPort),
+		Address: fmt.Sprintf("%s:%d", a.config.ConsulAddress, a.config.AgentPort),
 	})
 	if err != nil {
 		return err
 	}
-	s.Client = client
-	s.Catalog = client.Catalog()
-	s.Agent = client.Agent()
-	s.KV = client.KV()
+	a.client = client
+	a.catalog = client.Catalog()
+	a.agent = client.Agent()
+	a.kv = client.KV()
 	return nil
 }
 
-func (s *Agent) formatCheckID() string {
-	return fmt.Sprintf("service:%s", s.Config.ServiceName)
+func (a *Agent) formatCheckID() string {
+	return fmt.Sprintf("service:%s", a.config.ServiceName)
 }
 
-func (s *Agent) formatPrefix() string {
-	return fmt.Sprintf("%s/%s/", s.Config.ServiceName, s.Config.Env)
+func (a *Agent) formatPrefix() string {
+	return fmt.Sprintf("%s/%s/", a.config.ServiceName, a.config.Env)
 }
 
-func (s *Agent) formatCredential(cred string) string {
-	return fmt.Sprintf("%s/%s/%s", s.Config.ServiceName, s.Config.Env, cred)
+func (a *Agent) formatCredential(cred string) string {
+	return fmt.Sprintf("%s/%s/%s", a.config.ServiceName, a.config.Env, cred)
 }
 
-func (s *Agent) replaceKey(key string) string {
+func (a *Agent) replaceKey(key string) string {
 	parts := strings.Split(key, keySeparator)
 
 	return parts[len(parts)-1]
