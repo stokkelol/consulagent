c718656fdba1b39baaa82c079e54079331d70f56
redwhitelol <redwhite@bigmir.net>
2019-07-30 13:59:36 +0300

    Add getPrivateIPV4 func

--- ip.go
+++ ip.go
@@ -1,24 +1,26 @@
 package consulagent
 
 import (
+	"errors"
 	"io/ioutil"
+	"net"
 	"net/http"
 )
 
 const awsMagicUrl = "http://169.254.169.254/latest/meta-data/local-ipv4"
 
 // GetPrivateIPV4 returns private ipv4 for EC2 instance
-func GetPrivateIPV4() (string, error) {
+func GetPrivateIPV4() (net.IP, error) {
 	client := http.Client{}
 
 	req, err := http.NewRequest("GET", awsMagicUrl, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer func() {
 		_ = resp.Body.Close()
@@ -26,8 +28,14 @@ func GetPrivateIPV4() (string, error) {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(body), nil
+	parsed := net.ParseIP(string(body))
+
+	if parsed == nil {
+		return nil, errors.New("malformed IPV4")
+	}
+
+	return parsed, nil
 }
